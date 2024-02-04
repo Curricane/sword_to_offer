6bfaed85a351051ab45a56cf7267a41b35923bdb
curricane <ccuricane@gmail.com>
2024-02-04 23:55:09 +0800

    add test case in test003

--- test003/src/main.rs
+++ test003/src/main.rs
@@ -2,10 +2,14 @@ fn main() {
     println!("Hello, world!");
 }
 
+trait Solution {
+    fn find_dupicate(nums: Vec<i32>) -> i32;
+}
+
 // 快慢指针
-struct Solution;
-impl Solution {
-    pub fn find_dupicate(nums: Vec<i32>) -> i32 {
+struct Impl1;
+impl Solution for Impl1 {
+    fn find_dupicate(nums: Vec<i32>) -> i32 {
         let mut slow = 0;
         let mut fast = 0;
         // 1. 快慢指针相遇
@@ -24,5 +28,18 @@ impl Solution {
             fast = nums[fast] as usize;
         }
         slow as i32
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_impl1() {
+        let nums = vec![3, 1, 3, 4, 2];
+        assert_eq!(Impl1::find_dupicate(nums), 3);
+        let nums = vec![1, 3, 4, 2, 2];
+        assert_eq!(Impl1::find_dupicate(nums), 2);
     }
 }
